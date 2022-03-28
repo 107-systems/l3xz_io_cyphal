60d21b2f0657812a8ad8adc3ea3c116da4d574f2
Alexander Entinger <cto@lxrobotics.com>
2022-03-28 08:48:34 +0200

    Providing meaningful arguments for scanner.

--- tooling/sweepctrl/sweepctrl.cpp
+++ tooling/sweepctrl/sweepctrl.cpp
@@ -11,6 +11,7 @@
 #include <string>
 #include <thread>
 #include <chrono>
+#include <atomic>
 #include <sstream>
 #include <iostream>
 
@@ -26,12 +27,17 @@ using namespace l3xz::driver;
 using namespace boost::program_options;
 
 /**************************************************************************************
+ * GLOBAL VARIABLES
+ **************************************************************************************/
+
+static std::atomic<int> scan_cnt = 0;
+
+/**************************************************************************************
  * FUNCTIONS
  **************************************************************************************/
 
 void sweep_scan_complete(SweepThread::ScanDataVect const & scan)
 {
-  static int scan_cnt = 0;
   std::cout << "Scan #" << scan_cnt << std::endl
             << "[" << std::endl;
   scan_cnt++;
@@ -50,110 +56,75 @@ int main(int argc, char **argv)
 {
   try
   {
-    SweepThread sweep_thd("/dev/ttyUSB0", 1, 500, sweep_scan_complete);
-    l3xz::common::threading::ThreadBase::stats(std::cout);
-    /*
-    std::cout << "before setup" << std::endl;
-    sweep_thd.setup();
-    std::cout << "after setup" << std::endl;
-    */
-    for(;;) std::this_thread::sleep_for (std::chrono::seconds(1));
-    /*
-    for(;;)
+    std::string param_device_name;
+    int param_baudrate;
+    int param_rpm;
+    int param_sample_rate;
+    int param_num_scans;
+
+    options_description desc("Allowed options");
+    desc.add_options()
+      ("help", "Show this help message.")
+      ("device", value<std::string>(&param_device_name)->required(), "Sweep device name, i.e. /dev/ttyUSB0.")
+      ("baud", value<int>(&param_baudrate)->default_value(115200), "Sweep baud rate.")
+      ("rpm",
+       value<int>(&param_rpm)->notifier([](int const val)
+                                          {
+                                            if(val < 0 || val > 10)
+                                              throw validation_error(validation_error::invalid_option_value, "rpm", std::to_string(val));
+                                          })->required(),
+       "Scanner rotational speed (0 - 10 RPM)")
+      ("sample-rate",
+       value<int>(&param_sample_rate)->notifier([](int const val)
+                                                  {
+                                                   if(val != 500 && val != 750 && val != 1000)
+                                                       throw validation_error(validation_error::invalid_option_value, "sample-rate", std::to_string(val));
+                                                  })->required(),
+       "Scanner sample rate (measurements / second) (500,750,1000).")
+      ("num-scans",
+       value<int>(&param_num_scans)->default_value(10),
+       "Number of scans before the driver shuts down.")
+      ;
+
+    variables_map vm;
+    store(command_line_parser(argc, argv).options(desc).run(), vm);
+
+    /**************************************************************************************
+     * --help
+     **************************************************************************************/
+
+    if (vm.count("help"))
     {
-      std::cout << "before loop" << std::endl;
-      sweep_thd.loop();
-      std::cout << "after loop" << std::endl;
+      std::cout << "Usage: sweepctrl [options]\n";
+      std::cout << desc;
+      return EXIT_SUCCESS;
     }
-    */
-    return EXIT_SUCCESS;
+
+    notify(vm);
+
+    /**************************************************************************************
+     * ... do the scan again ...
+     **************************************************************************************/
+
+    try
+    {
+      SweepThread sweep_thd(param_device_name, param_rpm, param_sample_rate, sweep_scan_complete);
+      l3xz::common::threading::ThreadBase::stats(std::cout);
+
+      while (scan_cnt < param_num_scans)
+        std::this_thread::sleep_for (std::chrono::milliseconds(10));
+    }
+    catch(sweep::device_error const & e)
+    {
+      std::cerr << e.what() << std::endl;
+      return EXIT_FAILURE;
+    }
   }
-  catch(sweep::device_error const & e)
+  catch(std::exception const & e)
   {
     std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
   }
+
+  return EXIT_SUCCESS;
 }
-
-//   try
-//   {
-//     std::string param_device_name;
-//     int param_baudrate;
-//     int param_channel;
-//     int param_pulse_width_us;
-//     int param_move_time_ms;
-
-//     options_description desc("Allowed options");
-//     desc.add_options()
-//       ("help", "Show this help message.")
-//       ("device", value<std::string>(&param_device_name)->required(), "SSC32U servo controller device name, i.e. /dev/ttyUSB0.")
-//       ("baud", value<int>(&param_baudrate)->default_value(115200), "SSC32U servo controller baud rate.")
-//       ("move", "Move a single servo.")
-//       ("channel",
-//        value<int>(&param_channel)->notifier([](int const val)
-//                                             {
-//                                               if(val < 0 || val > 31)
-//                                                 throw validation_error(validation_error::invalid_option_value, "channel", std::to_string(val));
-//                                             }),
-//        "Servo channel (0 - 31).")
-//       ("pulse",
-//        value<int>(&param_pulse_width_us)->notifier([](int const val)
-//                                                    {
-//                                                     if(val < 500 || val > 2500)
-//                                                         throw validation_error(validation_error::invalid_option_value, "pulse", std::to_string(val));
-//                                                    }),
-//        "Servo pulse width / us (500 - 2500).")
-//       ("time",
-//        value<int>(&param_move_time_ms)->notifier([](int const val)
-//                                                  {
-//                                                   if(val < 0 || val > 65535)
-//                                                       throw validation_error(validation_error::invalid_option_value, "time", std::to_string(val));
-//                                                  }),
-//        "Servo travel time / ms (0 - 65535).")
-//       ;
-
-//     variables_map vm;
-//     store(command_line_parser(argc, argv).options(desc).run(), vm);
-
-//     /**************************************************************************************
-//      * --help
-//      **************************************************************************************/
-
-//     if (vm.count("help"))
-//     {
-//       std::cout << "Usage: ssc32ctrl [options]\n";
-//       std::cout << desc;
-//       return EXIT_SUCCESS;
-//     }
-
-//     notify(vm);
-
-//     SSC32 ssc32_ctrl(param_device_name, param_baudrate);
-  
-//     /**************************************************************************************
-//      * --move
-//      **************************************************************************************/
-
-//     if (vm.count("move"))
-//     {
-//       if (!vm.count("channel")) {
-//         std::cerr << "Error, specify servo channel via '--channel'" << std::endl;
-//         return EXIT_FAILURE;
-//       }
-//       if (!vm.count("pulse")) {
-//         std::cerr << "Error, specify servo pulse width via '--pulse'" << std::endl;
-//         return EXIT_FAILURE;
-//       }
-//       if (!vm.count("time")) {
-//         std::cerr << "Error, specify servo travel time via '--time'" << std::endl;
-//         return EXIT_FAILURE;
-//       }
-
-//       ssc32_ctrl.setPulseWidth(param_channel, param_pulse_width_us, param_move_time_ms);
-//     }
-//   }
-//   catch(std::exception const & e)
-//   {
-//     std::cerr << e.what() << std::endl;
-//     return EXIT_FAILURE;
-//   }
