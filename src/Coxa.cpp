8cfc371babce46af74103d27dd08c4fd3d8ebe8f
Alexander Entinger <cto@lxrobotics.com>
2022-03-28 13:38:19 +0200

    Implement abstraction for manipulating coxa joint servos via Leg ID.

--- src/Coxa.cpp
+++ src/Coxa.cpp
@@ -4,69 +4,69 @@
  * Contributors: https://github.com/107-systems/107-Arduino-UAVCAN/graphs/contributors.
  */
 
-#ifndef DYNAMIXEL_MX28_H_
-#define DYNAMIXEL_MX28_H_
-
 /**************************************************************************************
- * INCLUDE
+ * INCLUDES
  **************************************************************************************/
 
-#include <memory>
+#include <l3xz/Coxa.h>
 
-#include <l3xz/driver/dynamixel/Dynamixel.h>
+#include <map>
 
 /**************************************************************************************
  * NAMESPACE
  **************************************************************************************/
 
-namespace l3xz::driver
+namespace l3xz
 {
 
 /**************************************************************************************
- * CLASS DECLARATION
+ * GLOBAL CONSTANTS
  **************************************************************************************/
 
-class MX28
+static std::map<Leg, driver::Dynamixel::Id> COXA_MAP =
 {
-public:
-
-  MX28(std::shared_ptr<Dynamixel> dyn_ctrl);
-
-  std::optional<Dynamixel::IdVect> discover();
-
-
-  void turnLedOn (Dynamixel::IdVect const & id_vect);
-  void turnLedOff(Dynamixel::IdVect const & id_vect);
-
-  void torqueOn (Dynamixel::Id const id);
-  void torqueOn (Dynamixel::IdVect const & id_vect);
-  void torqueOff(Dynamixel::IdVect const & id_vect);
-
-  typedef std::tuple<Dynamixel::Id, float> AngleData;
-  typedef std::vector<AngleData> AngleDataVect;
-
-  std::optional<AngleData> getAngle(Dynamixel::Id const id);
-  AngleDataVect            getAngle(Dynamixel::IdVect const & id_vect);
-
-  bool setAngle(AngleData const & angle_data);
-  bool setAngle(AngleDataVect const & angle_data_vect);
-
-
-private:
-
-  std::shared_ptr<Dynamixel> _dyn_ctrl;
+  {Leg::FrontLeft,   1},
+  {Leg::FrontRight,  2},
+  {Leg::MiddleLeft,  3},
+  {Leg::MiddleRight, 4},
+  {Leg::BackLeft,    5},
+  {Leg::BackRight,   6},
 };
 
 /**************************************************************************************
- * TYPEDEF
+ * CTOR/DTOR
  **************************************************************************************/
 
-typedef std::shared_ptr<MX28> SharedMX28;
+Coxa::Coxa(driver::SharedMX28 & mx28)
+: _mx28{mx28}
+{
+
+}
+
+/**************************************************************************************
+ * PUBLIC MEMBER FUNCTIONS
+ **************************************************************************************/
+
+void Coxa::set(Leg const leg, float const angle_deg)
+{
+  driver::Dynamixel::Id const id = COXA_MAP.at(leg);
+  driver::MX28::AngleData const angle_data = std::make_tuple(id, angle_deg);
+  _mx28->setAngle(angle_data);
+}
+
+std::optional<float> Coxa::get(Leg const leg)
+{
+  driver::Dynamixel::Id const id = COXA_MAP.at(leg);
+  std::optional<driver::MX28::AngleData> const opt_angle_data = _mx28->getAngle(id);
+  if (!opt_angle_data)
+    return std::nullopt;
+
+  auto [i,angle_deg] =(*opt_angle_data);
+  return angle_deg;
+}
 
 /**************************************************************************************
  * NAMESPACE
  **************************************************************************************/
 
-} /* l3xz::driver */
-
-#endif /* DYNAMIXEL_MX28_H_ */
+} /* l3xz */
