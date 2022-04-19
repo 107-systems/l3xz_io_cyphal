313e1526fd6fa5dce5cb42de79a5c536e5b47373
Alexander Entinger <cto@lxrobotics.com>
2022-04-19 16:05:57 +0200

    Moving dynamixel initialisation code into subfunction init_dynamixel.

--- src/node.cpp
+++ src/node.cpp
@@ -9,6 +9,7 @@
  **************************************************************************************/
 
 #include <string>
+#include <sstream>
 
 #include <ros/ros.h>
 #include <ros/console.h>
@@ -17,6 +18,12 @@
 
 #include <l3xz/driver/dynamixel/MX28.h>
 #include <l3xz/driver/dynamixel/Dynamixel.h>
+
+/**************************************************************************************
+ * FUNCTION DECLARATION
+ **************************************************************************************/
+
+bool init_dynamixel(l3xz::driver::SharedMX28 & mx28_ctrl);
 
 /**************************************************************************************
  * CONSTANT
@@ -37,25 +44,18 @@ int main(int argc, char **argv)
   ros::NodeHandle node_handle;
 
   std::shared_ptr<l3xz::driver::Dynamixel> dynamixel_ctrl = std::make_shared<l3xz::driver::Dynamixel>(DYNAMIXEL_DEVICE_NAME, DYNAMIXEL_PROTOCOL_VERSION, DYNAMIXEL_BAUD_RATE);
-  std::unique_ptr<l3xz::driver::MX28> mx28_ctrl(new l3xz::driver::MX28(dynamixel_ctrl));
+  l3xz::driver::SharedMX28 mx28_ctrl = std::make_shared<l3xz::driver::MX28>(dynamixel_ctrl);
 
-  std::optional<l3xz::driver::Dynamixel::IdVect> opt_id_vect =  mx28_ctrl->discover();
-  if (!opt_id_vect)
-    ROS_ERROR("Zero MX-28 servos detected.");
-  else
-  {
-    ROS_INFO("Detected Dynamixel MX-28:");
-    for (uint8_t id : opt_id_vect.value())
-      ROS_INFO("[ID:%d]", id);
-  }
+  if (!init_dynamixel(mx28_ctrl))
+    ROS_ERROR("init_dynamixel failed.");
 
-  mx28_ctrl->torqueOn(opt_id_vect.value());
+  //mx28_ctrl->torqueOn(opt_id_vect.value());
 
   ros::Rate loop_rate(1);
   while (ros::ok())
   {
     ros::spinOnce();
-
+/*
     mx28_ctrl->turnLedOn(opt_id_vect.value());
     loop_rate.sleep();
     mx28_ctrl->turnLedOff(opt_id_vect.value());
@@ -84,7 +84,44 @@ int main(int argc, char **argv)
 
     if (!mx28_ctrl->setAngle(angle_vect_set))
       ROS_ERROR("setAngle() failed");
+*/
   }
 
   return EXIT_SUCCESS;
+}
+
+/**************************************************************************************
+ * FUNCTION DECLARATION
+ **************************************************************************************/
+
+bool init_dynamixel(l3xz::driver::SharedMX28 & mx28_ctrl)
+{
+  std::optional<l3xz::driver::Dynamixel::IdVect> opt_act_id_vect = mx28_ctrl->discover();
+
+  if (!opt_act_id_vect) {
+    ROS_ERROR("Zero MX-28 servos detected.");
+    return false;
+  }
+
+  std::stringstream act_id_list;
+  for (auto id : opt_act_id_vect.value())
+    act_id_list << static_cast<int>(id) << " ";
+  ROS_INFO("Detected Dynamixel MX-28: { %s}", act_id_list.str().c_str());
+
+  bool all_req_id_found = true;
+  l3xz::driver::Dynamixel::IdVect const REQUIRED_ID_VECT{1,2,3,4,5,6,7,8};
+  for (auto req_id : REQUIRED_ID_VECT)
+  {
+    bool const req_id_found = std::count(opt_act_id_vect.value().begin(),
+                                         opt_act_id_vect.value().end(),
+                                         req_id) > 0;
+    if (!req_id_found) {
+      all_req_id_found = false;
+      ROS_ERROR("Unable to detect required dynamixel with node id %d", static_cast<int>(req_id));
+    }
+  }
+  if (!all_req_id_found)
+    return false;
+
+  return true;
 }
