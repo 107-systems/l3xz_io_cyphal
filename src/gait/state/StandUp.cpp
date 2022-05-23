e5810cd0dc3ccdc62069eded2652ea36a1992354
Alexander Entinger <cto@lxrobotics.com>
2022-05-23 10:23:10 +0200

    Very pre-eliminary IK Solver approach integrated into StandUp state.

--- src/gait/state/StandUp.cpp
+++ src/gait/state/StandUp.cpp
@@ -38,6 +38,45 @@ void StandUp::onExit()
 
 StateBase * StandUp::update(common::kinematic::Engine const & engine, GaitControllerInput & input, GaitControllerOutput & output)
 {
+  static double const TARGET_TIBIA_TIP_x = 100.0;
+  static double const TARGET_TIBIA_TIP_y = 100.0;
+  static double const TARGET_TIBIA_TIP_z = 100.0;
+
+  static std::list<Leg> const LEG_LIST =
+  {
+    Leg::LeftFront, Leg::LeftMiddle, Leg::LeftBack, Leg::RightFront, Leg::RightMiddle, Leg::RightBack
+  };
+
+  double x = TARGET_TIBIA_TIP_x,
+         y = TARGET_TIBIA_TIP_y,
+         z = TARGET_TIBIA_TIP_z;
+
+  for (auto leg : LEG_LIST)
+  {
+    /* Calculate required target angles for desired
+     * target position and set the output actuators.
+     */
+    double const coxa_deg_actual  = input(leg, Joint::Coxa )->get().value();
+    double const femur_deg_actual = input(leg, Joint::Femur)->get().value();
+    double const tibia_deg_actual = input(leg, Joint::Tibia)->get().value();
+
+    common::kinematic::IK_Input const ik_input(x, y, z, coxa_deg_actual, femur_deg_actual, tibia_deg_actual);
+
+    auto const ik_output = engine.ik_solve(ik_input);
+
+    if (!ik_output.has_value()) {
+      ROS_ERROR("StandUp::update, engine.ik_solve failed for (%0.2f, %0.2f, %0.2f / %0.2f, %0.2f, %0.2f)",
+        x, y, z, coxa_deg_actual, femur_deg_actual, tibia_deg_actual);
+      return this;
+    }
+
+    /* Set output to the angle actuators.
+     */
+    output(leg, Joint::Coxa )->set(ik_output.value().angle_deg(Joint::Coxa));
+    output(leg, Joint::Femur)->set(ik_output.value().angle_deg(Joint::Femur));
+    output(leg, Joint::Tibia)->set(ik_output.value().angle_deg(Joint::Tibia));
+  }
+
   return this;
 }
 
