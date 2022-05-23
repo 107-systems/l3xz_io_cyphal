80524370d85493e28e173e6ce5249bfae3f0bc79
Alexander Entinger <cto@lxrobotics.com>
2022-05-23 10:48:20 +0200

    Provide a LEG_JOINT list for accessing all sensor/actuators via ranged-based for loop.

--- include/Const.h
+++ include/Const.h
@@ -24,4 +24,26 @@ static std::list<Leg> const LEG_LIST =
   Leg::LeftFront, Leg::LeftMiddle, Leg::LeftBack, Leg::RightFront, Leg::RightMiddle, Leg::RightBack
 };
 
+static std::list<LegJointPair> const LEG_JOINT_LIST =
+{
+  std::tuple(Leg::LeftFront,   Joint::Coxa),
+  std::tuple(Leg::LeftFront,   Joint::Femur),
+  std::tuple(Leg::LeftFront,   Joint::Tibia),
+  std::tuple(Leg::LeftMiddle,  Joint::Coxa),
+  std::tuple(Leg::LeftMiddle,  Joint::Femur),
+  std::tuple(Leg::LeftMiddle,  Joint::Tibia),
+  std::tuple(Leg::LeftBack,    Joint::Coxa),
+  std::tuple(Leg::LeftBack,    Joint::Femur),
+  std::tuple(Leg::LeftBack,    Joint::Tibia),
+  std::tuple(Leg::RightBack,   Joint::Coxa),
+  std::tuple(Leg::RightBack,   Joint::Femur),
+  std::tuple(Leg::RightBack,   Joint::Tibia),
+  std::tuple(Leg::RightMiddle, Joint::Coxa),
+  std::tuple(Leg::RightMiddle, Joint::Femur),
+  std::tuple(Leg::RightMiddle, Joint::Tibia),
+  std::tuple(Leg::RightFront,  Joint::Coxa),
+  std::tuple(Leg::RightFront,  Joint::Femur),
+  std::tuple(Leg::RightFront,  Joint::Tibia),
+};
+
 #endif /* CONST_H_ */
