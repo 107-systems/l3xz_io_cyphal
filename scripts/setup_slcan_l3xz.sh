bc7100e3f861cb6d4292707eb4e9558c1be22601
Pavel Kirienko <pavel.kirienko@gmail.com>
2022-05-31 11:27:14 +0300

    Add missing sudo

--- scripts/setup_slcan_l3xz.sh
+++ scripts/setup_slcan_l3xz.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd $(dirname $(readlink -f "$0"))
-./setup_slcan.sh --remove-all --basename can --speed-code 5 \
+sudo ./setup_slcan.sh --remove-all --basename can --speed-code 5 \
   /dev/serial/by-id/usb-Zubax_Robotics_Zubax_Babel_2300390005514E433734382000000000-if00 \
   /dev/serial/by-id/usb-Zubax_Robotics_Zubax_Babel_26003A000B5150423339302000000000-if00
