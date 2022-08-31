3ee759ef2de5bed0d20a96ca60f0b51285aedf9a
Alexander Entinger <cto@lxrobotics.com>
2022-08-31 08:16:07 +0200

    Split NodeMonitor in header/source file.

--- include/l3xz_io/phy/opencyphal/NodeMonitor.h
+++ include/l3xz_io/phy/opencyphal/NodeMonitor.h
@@ -30,80 +30,14 @@ namespace phy::opencyphal
 class NodeMonitor
 {
 public:
-  NodeMonitor(phy::opencyphal::Node & node, rclcpp::Logger const logger, std::list<CanardNodeID> const node_id_list)
-  : _mtx{}
-  {
-    for (auto node_id : node_id_list)
-      _node_heartbeat_data[node_id] = HeartbeatData();
+  NodeMonitor(Node & node, rclcpp::Logger const logger, std::list<CanardNodeID> const node_id_list);
 
-    if (!subscribeHeartbeat(node))
-      RCLCPP_ERROR(logger, "failed to subscribe to 'hearbeat'");
-  }
+  static std::string toStr(std::list<CanardNodeID> const & node_list);
+  std::list<CanardNodeID> detectedNodeIdList();
 
-  static std::string toStr(std::list<CanardNodeID> const & node_list)
-  {
-    std::stringstream ss;
-    for (auto node_id : node_list)
-      ss << static_cast<int>(node_id) << " ";
-    return ss.str();
-  }
-
-  std::list<CanardNodeID> detectedNodeIdList()
-  {
-    std::lock_guard<std::mutex> lock(_mtx);
-    return _detected_node_id_list;
-  }
-
-  std::tuple<bool, std::list<CanardNodeID>> isConnected(std::chrono::seconds const heartbeat_timeout)
-  {
-    std::lock_guard<std::mutex> lock(_mtx);
-
-    bool is_connected = true;
-    std::list<CanardNodeID> node_list;
-
-    for (auto [node_id, data] : _node_heartbeat_data)
-      if(!data.isConnected(heartbeat_timeout))
-      {
-        is_connected = false;
-        node_list.push_back(node_id);
-      }
-    
-    return std::tuple(is_connected, node_list);
-  }
-
-  std::tuple<bool, std::list<CanardNodeID>> isHealthy()
-  {
-    std::lock_guard<std::mutex> lock(_mtx);
-
-    bool is_healty = true;
-    std::list<CanardNodeID> node_list;
-
-    for (auto [node_id, data] : _node_heartbeat_data)
-      if(!data.isHealthy())
-      {
-        is_healty = false;
-        node_list.push_back(node_id);
-      }
-    
-    return std::tuple(is_healty, node_list);
-  }
-
-  std::tuple<bool, std::list<CanardNodeID>> isOperational()
-  {
-    std::lock_guard<std::mutex> lock(_mtx);
-
-    bool is_operational = true;
-    std::list<CanardNodeID> node_list;
-
-    for (auto [node_id, data] : _node_heartbeat_data)
-      if(!data.isOperational())
-      {
-        is_operational = false;
-        node_list.push_back(node_id);
-      }
-    
-    return std::tuple(is_operational, node_list);
-  }
+  std::tuple<bool, std::list<CanardNodeID>> isConnected(std::chrono::seconds const heartbeat_timeout);
+  std::tuple<bool, std::list<CanardNodeID>> isHealthy();
+  std::tuple<bool, std::list<CanardNodeID>> isOperational();
 
 private:
 
@@ -132,28 +66,7 @@ private:
   std::map<CanardNodeID, HeartbeatData> _node_heartbeat_data;
   std::list<CanardNodeID> _detected_node_id_list;
 
-  bool subscribeHeartbeat(phy::opencyphal::Node & node)
-  {
-    return node.subscribe<uavcan::node::Heartbeat_1_0<>>(
-        [this](CanardRxTransfer const & transfer)
-        {
-          uavcan::node::Heartbeat_1_0<> const heartbeat = uavcan::node::Heartbeat_1_0<>::deserialize(transfer);
-            
-          std::lock_guard<std::mutex> lock(_mtx);
-
-          if (std::find(std::begin(_detected_node_id_list),
-                        std::end  (_detected_node_id_list),
-                        transfer.metadata.remote_node_id)
-              == std::end(_detected_node_id_list))
-          {
-            _detected_node_id_list.push_back(transfer.metadata.remote_node_id);
-            _detected_node_id_list.sort(); /* Make sure its in ascending order for better readability. */
-          }
-
-          HeartbeatData data(std::chrono::system_clock::now(), heartbeat.data.health, heartbeat.data.mode);
-          _node_heartbeat_data[transfer.metadata.remote_node_id] = data;
-        });
-  }
+  bool subscribeHeartbeat(Node & node);
 };
 
 /**************************************************************************************
---
+++ src/phy/opencyphal/NodeMonitor.cpp
@@ -0,0 +1,130 @@
+/**
+ * Copyright (c) 2022 LXRobotics GmbH.
+ * Author: Alexander Entinger <alexander.entinger@lxrobotics.com>
+ * Contributors: https://github.com/107-systems/l3xz_io/graphs/contributors.
+ */
+
+/**************************************************************************************
+ * INCLUDES
+ **************************************************************************************/
+
+#include <l3xz_io/phy/opencyphal/NodeMonitor.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace phy::opencyphal
+{
+
+/**************************************************************************************
+ * CTOR/DTOR
+ **************************************************************************************/
+
+NodeMonitor::NodeMonitor(Node & node, rclcpp::Logger const logger, std::list<CanardNodeID> const node_id_list)
+: _mtx{}
+{
+  for (auto node_id : node_id_list)
+    _node_heartbeat_data[node_id] = HeartbeatData();
+
+  if (!subscribeHeartbeat(node))
+    RCLCPP_ERROR(logger, "failed to subscribe to 'hearbeat'");
+}
+
+std::string NodeMonitor::toStr(std::list<CanardNodeID> const & node_list)
+{
+  std::stringstream ss;
+  for (auto node_id : node_list)
+    ss << static_cast<int>(node_id) << " ";
+  return ss.str();
+}
+
+std::list<CanardNodeID> NodeMonitor::detectedNodeIdList()
+{
+  std::lock_guard<std::mutex> lock(_mtx);
+  return _detected_node_id_list;
+}
+
+std::tuple<bool, std::list<CanardNodeID>> NodeMonitor::isConnected(std::chrono::seconds const heartbeat_timeout)
+{
+  std::lock_guard<std::mutex> lock(_mtx);
+
+  bool is_connected = true;
+  std::list<CanardNodeID> node_list;
+
+  for (auto [node_id, data] : _node_heartbeat_data)
+    if(!data.isConnected(heartbeat_timeout))
+    {
+      is_connected = false;
+      node_list.push_back(node_id);
+    }
+    
+  return std::tuple(is_connected, node_list);
+}
+
+std::tuple<bool, std::list<CanardNodeID>> NodeMonitor::isHealthy()
+{
+  std::lock_guard<std::mutex> lock(_mtx);
+
+  bool is_healty = true;
+  std::list<CanardNodeID> node_list;
+
+  for (auto [node_id, data] : _node_heartbeat_data)
+    if(!data.isHealthy())
+    {
+      is_healty = false;
+      node_list.push_back(node_id);
+    }
+    
+  return std::tuple(is_healty, node_list);
+}
+
+std::tuple<bool, std::list<CanardNodeID>> NodeMonitor::isOperational()
+{
+  std::lock_guard<std::mutex> lock(_mtx);
+
+  bool is_operational = true;
+  std::list<CanardNodeID> node_list;
+
+  for (auto [node_id, data] : _node_heartbeat_data)
+    if(!data.isOperational())
+    {
+      is_operational = false;
+      node_list.push_back(node_id);
+    }
+
+  return std::tuple(is_operational, node_list);
+}
+
+/**************************************************************************************
+ * PRIVATE MEMBER FUNCTIONS
+ **************************************************************************************/
+
+bool NodeMonitor::subscribeHeartbeat(Node & node)
+{
+  return node.subscribe<uavcan::node::Heartbeat_1_0<>>(
+      [this](CanardRxTransfer const & transfer)
+      {
+        uavcan::node::Heartbeat_1_0<> const heartbeat = uavcan::node::Heartbeat_1_0<>::deserialize(transfer);
+            
+        std::lock_guard<std::mutex> lock(_mtx);
+
+        if (std::find(std::begin(_detected_node_id_list),
+                      std::end  (_detected_node_id_list),
+                      transfer.metadata.remote_node_id)
+            == std::end(_detected_node_id_list))
+        {
+          _detected_node_id_list.push_back(transfer.metadata.remote_node_id);
+          _detected_node_id_list.sort(); /* Make sure its in ascending order for better readability. */
+        }
+
+        HeartbeatData data(std::chrono::system_clock::now(), heartbeat.data.health, heartbeat.data.mode);
+        _node_heartbeat_data[transfer.metadata.remote_node_id] = data;
+      });
+}
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* phy::opencyphal */
